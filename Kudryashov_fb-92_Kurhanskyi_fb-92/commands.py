9e2bd6dab287c67d49893bcccb08086327d5f9c6
Leonid <lkurgan55@gmail.com>
2021-11-09 15:30:34 +0200

    second part

--- Kudryashov_fb-92_Kurhanskyi_fb-92/commands.py
+++ Kudryashov_fb-92_Kurhanskyi_fb-92/commands.py
@@ -1,7 +1,5 @@
 
 def create(name, collections_instances):
-    
-    
     # not unique name
     if name in collections_instances.keys():
         return collections_instances, name, -3
@@ -9,34 +7,56 @@ def create(name, collections_instances):
     collections_instances[name] = {}
     return collections_instances, name, 1
 
-def insert(name, text, collections_instances):
+def insert(name, text, collections_instances, inverted_indexes):
     index = 0
     # incorrect name        
     if collections_instances.get(name) == None:
-        return collections_instances, name, index, -5
+        return collections_instances, inverted_indexes, name, index, -5
     # getting new index
     indexes = [int(x) for x in collections_instances[name].keys()]
     index = str(int(max(indexes)) + 1) \
         if len(collections_instances[name].keys()) else '0'
     # adding text to collections
     collections_instances[name].update({index: text})
-    return collections_instances, name, index, 2
+    # creating inverted indexes
+    words = text.split()
 
-def remove(case, name, collections_instances, index = 0):
-    
+    for i in range(len(words)):
+        if words[i] in inverted_indexes.keys():
+            try: 
+                inverted_indexes[words[i]][name][index].append(i)
+            except: 
+                try: inverted_indexes[words[i]][name][index] = [i]
+                except: inverted_indexes[words[i]][name] = {index: [i]}
+                
+        else: # {"world": {colection1:{ doc1: [номер в тексте]: }
+            inverted_indexes[words[i]] = {name:{index:[i]}}
+
+    return collections_instances, inverted_indexes, name, index, 2
+
+def remove(case, name, collections_instances, inverted_indexes, index = 0):
+    index = str(index)
     # incorrect name        
     if collections_instances.get(name) == None:
-        return collections_instances, name, index, -5
+        return collections_instances, name, inverted_indexes, index, -5
     
-    if case == 1:
+    if case == 1: # removing colection {"world": {colection1:{ doc1: [номер в тексте]: }
         collections_instances.pop(name)
-        return collections_instances, name, index, 3
-    elif case == 2:
+        for word in inverted_indexes.keys():
+            try:
+                inverted_indexes[word].pop(name)
+            except: pass
+        return collections_instances, name, inverted_indexes, index, 3
+    elif case == 2: # removing doc from colection
         if collections_instances[name].get(index) == None:
-            return collections_instances, name, index, -11
+            return collections_instances, name, inverted_indexes, index, -11
         collections_instances[name].pop(index)
-        return collections_instances, name, index, 4
-        
+        for word in inverted_indexes.keys():
+            try:
+                inverted_indexes[word][name].pop(index)
+            except: pass
+
+        return collections_instances, name, inverted_indexes, index, 4
 
 def exit(command):
     pass
@@ -51,6 +71,12 @@ def search(name, condition, case, collections_instances):
 
     return collections_instances, name, 100
 
+def print_indexex(name, inverted_indexes):    
+    for word in inverted_indexes.keys():
+        if name not in inverted_indexes[word].keys(): continue
+        print(f'"{word}":')
+        for doc in inverted_indexes[word][name]:
+            print(f"{doc} -> {inverted_indexes[word][name][doc]}")
 
 def check_status(status, name ="", index = 0):
     statuses = {
--- Kudryashov_fb-92_Kurhanskyi_fb-92/main.py
+++ Kudryashov_fb-92_Kurhanskyi_fb-92/main.py
@@ -5,6 +5,7 @@ import myparser
 import commands
 
 collections_instances = {}
+inverted_indexes = {} # {"world": {colection1:{ doc1: [номер в тексте]: }
 
 # loading data
 if os.path.exists("./collections_instances.json"):
@@ -13,6 +14,11 @@ if os.path.exists("./collections_instances.json"):
 else:
     pass
 
+if os.path.exists("./inverted_indexes.json"):
+    with open("inverted_indexes.json", "r") as read_file:
+        inverted_indexes = json.load(read_file)
+else:
+    pass
 
 
 print("Enter commands")
@@ -21,6 +27,7 @@ while(True):
     status = 0
 
     print(collections_instances) # delete before release
+    print(inverted_indexes) # delete before release
     
     text = input(">>")
     text = myparser.cleaning_text(text)
@@ -43,14 +50,14 @@ while(True):
         name, text, status = myparser.parse_insert(text)
         commands.check_status(status)
         if status <= 0: continue # checking an error
-        collections_instances, name, index, status = commands.insert(name, text, collections_instances)
+        collections_instances, inverted_indexes, name, index, status = commands.insert(name, text, collections_instances, inverted_indexes)
         commands.check_status(status, name, index)
 
     elif command == "REMOVE":
         case, name, index, status = myparser.parse_remove(text)
         commands.check_status(status)
         if status <= 0: continue # checking an error
-        collections_instances, name, index, status = commands.remove(case, name, collections_instances, index)
+        collections_instances, name, inverted_indexes, index, status = commands.remove(case, name, collections_instances, inverted_indexes, index)
         commands.check_status(status)
     
 
@@ -69,8 +76,8 @@ while(True):
         name, status = myparser.parse_print_index(text)
         commands.check_status(status)
         if status < 0: continue
+        commands.print_indexex(name, inverted_indexes)
 
-        print(f"indexes from {name}")
     
     elif command == "SHOW":
         status = myparser.parse_show(text)
@@ -85,6 +92,8 @@ while(True):
         # saving data
         with open("collections_instances.json", "w") as write_file:
             json.dump(collections_instances, write_file)
+        with open("inverted_indexes.json", "w") as write_file:
+            json.dump(inverted_indexes, write_file)
         break
     else:
         commands.check_status(0)
